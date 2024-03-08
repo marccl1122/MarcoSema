7adffdb74007d8f4b4c9d7831207a4cdfc905fba
marccl1122 <156139163+marccl1122@users.noreply.github.com>
2024-03-08 15:04:15 -0800

    contact.html/ sass/ css
    Navigation Bar added.

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    alert(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+  });
